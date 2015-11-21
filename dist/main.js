bf5d84565f3652dcbdc93403f185c1cdd70b66ee
sunchenguang <809200299@qq.com>
2015-11-21 20:55:33 +0800

    optimize program structure

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: 孙晨光
- * @Date:   2015-11-21 18:07:16
- * @Last Modified by:   孙晨光
- * @Last Modified time: 2015-11-21 18:08:29
- */
-
-console.log('this is main.js');
--- gulpfile.js
+++ gulpfile.js
@@ -4,44 +4,69 @@
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')(),
     bs = require('browser-sync').create();
-
 var reload = bs.reload,
     runSequence = require('run-sequence').use(gulp),
-    src = './src', //源目录路径
-    dist = './dist'; //输出路径
-
-
+    src = './assets', //开发环境目录
+    build = './build'; //用于生产环境的目录
+//编译ES6语法的js
 gulp.task('babel', function() {
-    return gulp.src('./src/*.jsx')
+    return gulp.src(src + '/jsx/**/*.jsx')
         .pipe($.babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(src + '/js/'));
 });
-
-
-// Start the server
+// browserSync start the server
 gulp.task('bs', function() {
     var files;
-
     files = [
         src + '/**/*.+(html|php|js|css|png|jpg|svg|gif)'
     ];
-
     bs.init(files, {
         server: {
             baseDir: './'
         }
     });
 });
-
 gulp.task('server', ['babel', 'bs'], function() {
-    gulp.watch(src + '/**/*.jsx', function() {
+    gulp.watch(src + '/jsx/**/*.jsx', function() {
         runSequence('babel', reload);
     });
 });
-gulp.task('lint', function() {
-    return gulp.src('./dist/*.js')
+//处理脚本文件
+gulp.task('script', function() {
+    return gulp.src(src + '/js/**/*.js')
+        //先进行js hint
         .pipe($.jshint())
-        .pipe($.jshint.reporter('default'));
+        .pipe($.jshint.reporter('default'))
+        //再进行压缩
+        .pipe($.uglify())
+        //修改扩展名
+        .pipe($.rename(function(path) {
+            path.extname = ".min.js";
+        }))
+        .pipe(gulp.dest(build + '/js/'))
+        .pipe($.notify('Script process success!!!'));
+});
+//处理样式文件
+gulp.task('style', function() {
+    return gulp.src(src + '/sass/**/*.scss')
+        //scss编译
+        .pipe($.sass().on('error', $.sass.logError))
+        //添加浏览器前缀
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(src + '/css/'))
+        //css压缩
+        .pipe($.minifyCss({
+            compatibility: 'ie8'
+        }))
+        //修改扩展名
+        .pipe($.rename(function(path) {
+            path.extname = ".min.css";
+        }))
+        .pipe(gulp.dest(build + '/css/'))
+        .pipe($.notify('Style process success!!!'));
 });
